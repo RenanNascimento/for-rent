b85ac01df945cb87f80f3cf0a626c9ec7ec005cc
renan <renan.engcomp@gmail.com>
2021-05-31 17:58:04 -0300

    [SERVER] Add cors

--- server/app.py
+++ server/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import numpy as np
 import joblib
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/predict', methods=['POST'])
 def predict():
