0e4df2efd17d81a04503ac00f5113113000ee9a9
renan <renan.engcomp@gmail.com>
2021-07-12 21:09:48 -0300

    [APP] Add get response from predict API

--- app/src/components/Predict/Predict.js
+++ app/src/components/Predict/Predict.js
@@ -13,8 +13,9 @@ const Predict = () => {
       body: JSON.stringify({
         data: "[[0.99184781, 0.50659956, -0.07125115, 0.34301268, -0.89925291, -0.93238932, 0.10776269, 0.03825454, 0.24106124, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0]]"
       })
-  })
-    .then(res => console.log(res))
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
     .catch(err => console.log(err))
   }
 
